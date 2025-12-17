69afe5703cfc55b214205ea9ecfa4daa1ea30d61
Naumov German <albinos195@mail.ru>
2025-12-17 22:58:33 +0300

    fastapi v3

--- src/api/main.py
+++ src/api/main.py
@@ -1,11 +1,44 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 from typing import Optional
 from src.system.rag.pipeline import run
+from src.system.llm.llm_services import chat_vector_store_manager
 
 app = FastAPI(title="DS Navigator API")
+
+
+@app.get("/", tags=["Root"])
+async def root():
+    return {
+        "message": "Welcome to DS Navigator API",
+        "documentation": "/docs"
+    }
+
+
+@app.get("/health", status_code=status.HTTP_200_OK, tags=["Health"])
+async def healthcheck():
+    return {
+        "status": "ok"
+    }
+
+@app.get("/check-vdb", tags=["Health"])
+async def check_vdb():
+    try:
+        collection = chat_vector_store_manager.vector_store._collection
+        count = collection.count()
+
+        return {
+            "status": "ok",
+            "documents_in_vdb": count
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="vector db is not available"
+        )
 
 
 class ForwardRequest(BaseModel):
@@ -22,7 +55,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     )
 
 
-@app.post("/forward")
+@app.post("/forward", tags=["Usage"])
 async def forward(req: ForwardRequest):
     try:
         result = await run(
